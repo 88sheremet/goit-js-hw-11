dca7086bf90deb69ef4cb984b6c213fded93f0a6
Oleksandr Sheremet <88sheremet@gmail.com>
2022-12-04 18:33:28 +0200

    change

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,37 +1,6 @@
-// import axios from 'axios';
 
-// const BASE_URL = 'https://pixabay.com/api/';
-// const KEY = '31754006-f43a1b08b2cea32f92fc299f3';
-// const PARAM = '&orientation=horizontal&image_type=photo&safesearch=true';
+//  KEY = '31754006-f43a1b08b2cea32f92fc299f3';
 
-// export class PictureAPI {
-//   constructor() {
-//     this.searchQuery = '';
-//     this.page = 1;
-//     this.amountOfElements = 40;
-//   }
-
-//   async getPicture() {
-//     try {
-//       const res = await axios.get(
-//         `${BASE_URL}?key=${KEY}&q=${this.searchQuery}&page=${this.page}&per_page=${this.amountOfElements}${PARAM}`
-//       );
-//       //console.log(res.data);
-//       this.addPage();
-//       return res.data;
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   }
-
-//   addPage() {
-//     this.page += 1;
-//   }
-
-//   resetPage() {
-//     this.page = 1;
-//   }
-// }
 
 import axios from 'axios';
 
